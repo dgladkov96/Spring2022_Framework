6724acfdcecb30d17234de7e6f1ac4afd13bcdad
dgladkov96 <daniilg1996@gmail.com>
2022-05-15 19:35:36 -0400

    Landing + Test + Commmand Pages updated

---
+++ src/test/java/Pages/Darksky/LandingPage.java
@@ -0,0 +1,36 @@
+package Pages.Darksky;
+
+
+import Pages.Commands;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+
+public class LandingPage extends Commands {
+
+    // Locators of Landing Page elements
+    By timeMachineLocator = By.xpath("//a[text()='Time Machine' or text()='TIME MACHINE']");
+
+
+    // Method to interact with Landing Page elements
+    // method to scroll and find if Time Machine button is enabled
+
+
+
+    // method to scroll to Time Machine button
+    public WebElement scrollToTimeMachineButton() {
+        return scrollToElement(timeMachineLocator);
+    }
+
+    // method to check if Time Machine button is enabled
+    public boolean isTimeMachineButtonEnabled() {
+        return scrollToTimeMachineButton().isEnabled();
+    }
+
+
+    // method to check if Time Machine button is displayed
+
+
+    // method to click Time Machine button
+
+}
